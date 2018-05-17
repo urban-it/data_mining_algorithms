c38de4b2041b360723ade198966e878b3e31ff43
Conrad <grosserconrad@gmail.com>
2018-05-17 19:57:55 +0200

    Added counter

--- src/data_generators/randomi.py
+++ src/data_generators/randomi.py
@@ -1,6 +1,9 @@
 # Meet randomI, a random data generator for test data generation
 from random import randint
 
+import time
+from datetime import date
+
 def main(i, a, b):
 	for x in range(0, int(i)):
 		content = generateFile(a, b)
@@ -28,4 +31,11 @@ i = input("How many datapices would you like to generate?")
 a = input("How many rows should each datapiece have?")
 b = input("How long should each row be?")
 
+start_time = time.time()
+
 main(i, a, b)
+
+print("Data is generated. Have fun!")
+sec = time.time() - start_time
+minutes = sec / 60
+print("randomI took " + str(sec) + " seconds (" + str(minutes) + " minutes) for execution.")
