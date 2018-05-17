825e1514f17b3ddc6d4ee6f021510044bb8ba44a
Conrad <grosserconrad@gmail.com>
2018-05-17 19:35:21 +0200

    Joined dgen and randomi

--- src/data_generators/randomi.py
+++ src/data_generators/randomi.py
@@ -1,16 +1,27 @@
 # Meet randomI, a random data generator for test data generation
+from random import randint
 
 def main(i, a, b):
 	for x in range(0, int(i)):
-		generateFile(a, b)
+		content = generateFile(a, b)
+		writeFile(content, x)
 
 def generateFile(rows, lenghtOfRows):
-	for y in range(0, rows):
-		generateRow(lenghtOfRows)
+	content = []
+	for y in range(0, int(rows)):
+		content.append(generateRow(int(lenghtOfRows)))
+	return content
 
 def generateRow(lenghtOfRows):
+	row = ""
 	for z in range(0, lenghtOfRows):
-		line = line + generateChar()
+		row = row + str(randint(0, 9))
+	return row
+
+def writeFile(content, x):
+	file = open("testdata/file" + str(x) + ".txt", "w")
+	for w in range(0, len(content)):
+		file.write(content[w] + "\n")
 
 print("Hello World")
 i = input("How many datapices would you like to generate?")
